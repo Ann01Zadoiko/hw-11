5f6a7e2c29abf666f1f6441e2a1f8da7a3317c8b
Ann01Zadoiko <anna.zadoiko@gmail.com>
2022-07-06 06:42:36 +0300

    HW 11

--- src/main/java/Exercise1.java
+++ src/main/java/Exercise1.java
@@ -4,16 +4,27 @@ import java.util.function.Predicate;
 public class Exercise1 {
     private static List<String> names = Arrays.asList("Alisa", "Anton", "Victor", "Oleg", "Nataly", "Hosha");
 
-    public static List<String> sortNames(List<String> nam){
+    public static String sortNames(List<String> nam){
         Predicate<String> predicate = (n) -> (names.indexOf(n) %2) !=0;
-        List<String> result = new ArrayList<>();
+//        List<String> result = new ArrayList<>();
+//        for (int i = 0; i < nam.size(); i++) {
+//            if (predicate.test(nam.get(i))){
+//                result.add( i + ". " + nam.get(i) + " ");
+//            }
+//        }
+
+        String s = "";
         for (int i = 0; i < nam.size(); i++) {
             if (predicate.test(nam.get(i))){
-                result.add( i + ". " + nam.get(i) + " ");
+                s += i + ". " + nam.get(i) + " ";
             }
         }
 
-        return result;
+
+        return s.strip();
     }
 
+    public static void main(String[] args) {
+        System.out.println(sortNames(names));
+    }
 }
